bb2a9c907db8aa13354539bec6f798926b6f7528
gn-grc <genejogarcia.gg@gmail.com>
2020-04-04 12:35:17 +0800

    init

---
+++ index.php
@@ -0,0 +1,422 @@
+<!doctype html>
+<html>
+
+     <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+          <title></title>
+          
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          
+     </head>
+     
+     <body>
+          
+          <style>
+          
+               body{
+                    background-color: #F2F3F4;
+               }
+               
+               .hvr-skew-forward {
+                    display: inline-block;
+                    vertical-align: middle;
+                    -webkit-transform: perspective(1px) translateZ(0);
+                    transform: perspective(1px) translateZ(0);
+                    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+                    -webkit-transition-duration: 0.3s;
+                    transition-duration: 0.3s;
+                    -webkit-transition-property: transform;
+                    transition-property: transform;
+                    -webkit-transform-origin: 0 100%;
+                    transform-origin: 0 100%;
+               }
+               
+               .hvr-skew-forward:hover, .hvr-skew-forward:focus, .hvr-skew-forward:active {
+                    -webkit-transform: skew(-10deg);
+                    transform: skew(-10deg);
+               }
+               
+               
+               @-webkit-keyframes hvr-pulse-grow {
+                    to {
+                         -webkit-transform: scale(1.1);
+                         transform: scale(1.1);
+                    }
+               }
+               
+               @keyframes hvr-pulse-grow {
+                    to {
+                         -webkit-transform: scale(1.1);
+                         transform: scale(1.1);
+                    }
+               }
+               
+               .hvr-pulse-grow {
+                    display: inline-block;
+                    vertical-align: middle;
+                    -webkit-transform: perspective(1px) translateZ(0);
+                    transform: perspective(1px) translateZ(0);
+                    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+               }
+               
+               .hvr-pulse-grow:hover, .hvr-pulse-grow:focus, .hvr-pulse-grow:active {
+                    -webkit-animation-name: hvr-pulse-grow;
+                    animation-name: hvr-pulse-grow;
+                    -webkit-animation-duration: 0.3s;
+                    animation-duration: 0.3s;
+                    -webkit-animation-timing-function: linear;
+                    animation-timing-function: linear;
+                    -webkit-animation-iteration-count: infinite;
+                    animation-iteration-count: infinite;
+                    -webkit-animation-direction: alternate;
+                    animation-direction: alternate;
+               }
+          
+          </style>
+          
+          <!-- Start Content -->
+          
+          <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+               <a class="navbar-brand hvr-skew-forward" href="https://www.facebook.com/MINIMALISMO-491712891586566/" target="_blank">
+                    <h3 class="display-4" style="font-size:40px;"><i><b>MINIMALISMO</b></i></h3>
+               </a>
+
+               
+                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                      
+                   <ul class="navbar-nav">
+                    <li class="nav-item">
+                       <a class="nav-link hvr-pulse-grow" href="https://www.facebook.com/MINIMALISMO-491712891586566/" target="_blank">Facebook</a>
+                     </li>
+                     <li class="nav-item">
+                       <a class="nav-link hvr-pulse-grow" href="https://shopee.ph/minimalismo" target="_blank">Shopee</a>
+                     </li>
+                   </ul>
+                      
+                 </div>
+          </nav>
+          
+          <div class="container mb-5">
+          
+               <div class="row">
+               
+                    <div class="shadow-lg bg-light p-4 mt-5 col-lg-5">
+                         <h3 class="float-right">Order</h3>
+                                                  
+                         <button data-toggle="collapse" class="btn btn-success" data-target="#template">Template</button>
+
+                         <div id="template" class="collapse mb-3">
+                         
+                              <p class="mt-3"><span class="text-danger"> There must be no empty lines or a gap. </span></p>
+                              <p class="text-danger"> Extra spaces in a line are still considered to be valid.</p>
+                              <p class="text-danger">For the price and shipping fee, do not add letters (a - Z) because it can't be processed.</p>
+                              
+                              <textarea class="mt-3 form-control bg-white" rows="12" disabled>
+Full Name:
+Address:
+Contact Number:
+Design Code:
+Size:
+Quantity:
+Shirt Color:
+Shipping Option:
+Notes:
+Price:
+Shipping Fee:
+                              </textarea>
+                              
+                         </div>
+                         
+                         <button id="btnSubmit" onclick="encodeOrder()" class="btn btn-primary ">Submit</button>
+                         
+                         <textarea rows="13" class="mt-3 form-control" id="order-raw" placeholder="Ctrl+V / Paste"></textarea>
+
+                    </div>
+
+                    <div class="col-md-1"></div>
+                    
+                    <div class="shadow-lg bg-light p-4 mt-5 col-lg-6">
+
+                         <form>
+                         
+                              
+                              <div class="form-group">
+                                   <input type="submit" id="createBtn" name="createBtn" value="Create Image" class="ml-sm-3 text-left btn btn-primary" disabled>
+                                   
+                                   <h3 class="float-right">Order Information</h3>
+                              </div>
+                              
+                              <div class="form-group col-md-12 mt-2">
+
+                                   <label for="orderNumber">Order Number</label>
+                                   <input type="text" name="orderNumber" id="orderNumber" class="form-control" required>
+
+                              </div>
+                              
+                              <div class="form-inline ">
+
+                                   <div class="form-group col-sm-6">
+
+                                        <label for="date" class="mr-sm-2">Date </label>
+                                        <input type="text" name="date" class="form-control bg-white" id="date">
+
+                                   </div>
+
+                                   <br>
+
+                                   <div class="form-group col-sm-6">
+
+                                        <label for="time" class="mr-sm-2">Time
+                                        </label>
+                                        <input type="text" id="time" class="form-control bg-white" name="time" >
+
+                                   </div>
+
+                              </div>
+
+                              <div class="form-group col-md-12 mt-2">
+
+                                   <label for="fullName">Full Name</label>
+                                   <input type="text" name="fullName" id="fullName" class="form-control">
+
+                              </div>
+                         
+                             <div class="form-group col-md-12 mt-2">
+
+                                   <label for="totalAmount">Total Amount</label>
+                                   <input type="text" name="totalAmount" id="totalAmount" class="form-control">
+
+                              </div>
+
+                              <input type="button" data-toggle="collapse" class="btn btn-success ml-sm-3" data-target="#detailed-information" value="More information...">
+                              
+                              <div id="detailed-information" class="collapse shadow-sm bg-white border p-4 mt-5 col-md-12">
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="address">Address</label>
+                                        <input type="text" name="address" id="address" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="contactNumber">Contact Number</label>
+                                        <input type="text" name="contactNumber" id="contactNumber" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="designCode">Design Code</label>
+                                        <input type="text" name="designCode" id="designCode" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-inline">
+
+                                        <div class="form-group col-sm-6">
+
+                                             <label for="size" class="mr-sm-2">Size </label>
+                                             <input type="text" name="size" class="form-control" id="size" >
+
+                                        </div>
+
+                                        <br>
+
+                                        <div class="form-group col-sm-6">
+
+                                             <label for="quantity" class="mr-sm-2">Quantity
+                                             </label>
+                                             <input type="text" id="quantity" class="form-control" name="quantity">
+
+                                        </div>
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="shirtColor">Shirt Color</label>
+                                        <input type="text" name="shirtColor" id="shirtColor" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="shippingOption">Shipping Option</label>
+                                        <input type="text" name="shippingOption" id="shippingOption" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="notes">Notes</label>
+                                        <input type="text" name="notes" id="notes" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="price">Price</label>
+                                        <input type="text" name="price" id="price" class="form-control">
+
+                                   </div>
+
+                                   <div class="form-group col-md-12 mt-2">
+
+                                        <label for="shippingFee">Shipping Fee</label>
+                                        <input type="text" name="shippingFee" id="shippingFee" class="form-control">
+
+                                   </div>
+                              </div>
+
+                         </form>
+                    </div>
+
+               </div>
+
+          </div>
+          
+          <!-- E n d Content -->
+          
+          <script>
+               
+               let timeFld = document.getElementById('time');
+               let dateFld = document.getElementById('date');
+               
+               var today = new Date();
+               let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+               let date = (today.getMonth()+1) + '-' + today.getDate() + '-' + today.getFullYear();
+               
+               timeFld.value = time;
+               dateFld.value = date;
+               
+               var textarea = document.getElementById("order-raw");
+
+               var informationRows = []
+
+               function encodeOrder()
+               {
+                    let lines = textarea.value.split('\n');
+                    for(var i = 0;i < lines.length;i++){
+                         informationRows[i] = lines[i].split(':')[1].trim();
+                    }
+
+                    fillFields();
+                    
+                    //enable createBtn
+                    if(!textarea.value == '')
+                    {
+                         document.getElementById('createBtn').disabled = false;
+                    }
+               }
+
+               function fillFields()
+               {
+                    document.getElementById('fullName')          .value = informationRows[0];         
+                    document.getElementById('address')           .value = informationRows[1];   
+                    document.getElementById('contactNumber')     .value = informationRows[2];   
+                    document.getElementById('designCode')        .value = informationRows[3];         
+                    document.getElementById('size')              .value = informationRows[4];
+                    document.getElementById('quantity')          .value = informationRows[5];
+                    document.getElementById('shirtColor')        .value = informationRows[6];
+                    document.getElementById('shippingOption')    .value = informationRows[7];
+                    document.getElementById('notes')             .value = informationRows[8];
+                    document.getElementById('price')             .value = informationRows[9];
+                    document.getElementById('shippingFee')       .value = informationRows[10];
+                    document.getElementById('totalAmount')       .value = parseInt(informationRows[9]) + parseInt(informationRows[10]);
+
+               }
+     
+          </script>
+          
+          <?php
+
+          if(isset($_GET['createBtn'])){
+               
+               $date = $_GET['date'];
+               $time = $_GET['time'];
+               
+               $output ='Orders/' . $date . '_' . $_GET['fullName'] . '.jpg';
+
+               $imgsource = "data_sheet.jpg";
+               $formsamp = imagecreatefromjpeg($imgsource);
+
+               $black = imagecolorallocate($formsamp, 0, 0, 0);
+               $fontsize = 60;
+               $rotation = 0;
+               $font = __DIR__ . '/TCCEB.TTF';
+
+               $x = 1960; 
+               $y = 120;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['orderNumber']);
+               
+               $x = 660;
+               $y = 240;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['date']);
+               
+               $x = 1960;
+               $y = 240;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['time']);
+               
+               $x = 660;
+               $y = 340;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['fullName']);
+               
+               $x = 660;
+               $y = 500;		
+               imagettftext($formsamp, 40, $rotation, $x, $y, $black, $font, $_GET['address']);
+               
+               $x = 660;
+               $y = 680;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['contactNumber']);
+               
+               $x = 660;
+               $y = 780;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['designCode']);
+               
+               $x = 660;
+               $y = 900;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['size']);
+               
+               $x = 1960;
+               $y = 900;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['quantity']);
+               
+               $x = 660;
+               $y = 1020;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['shirtColor']);
+               
+               $x = 660;
+               $y = 1140;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['shippingOption']);
+               
+               $x = 660;
+               $y = 1260;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['notes']);
+               
+               $x = 660;
+               $y = 1380;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['price']);
+               
+               $x = 660;
+               $y = 1480;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['shippingFee']);
+               
+               $x = 660;
+               $y = 1580;		
+               imagettftext($formsamp, $fontsize, $rotation, $x, $y, $black, $font, $_GET['totalAmount']);
+
+               imagejpeg($formsamp, $output);
+          }
+
+          ?>
+          
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          
+     </body>
+     
+</html>
